5362a162ab71d60ad3572a82370cf18db6562109
Joe Cavanagh <jcavanagh617@gmail.com>
2014-09-22 22:04:40 -0500

    Small hypergeometric statistics lib

---
+++ analytics/lib.js
@@ -0,0 +1,58 @@
+/* global _ */
+//Stats are fun!
+
+//Factorial
+//TODO: Memoize
+this.factorial = function(n) {
+    var result = 1;
+    for (var i = 2; i <= n; i++) {
+        result *= i;
+    }
+
+    return result;
+};
+
+//Binomial coefficient
+//From a set of size n, return number of distinct subsets of size k
+//Positive integers only
+this.binomialCoeff = function(n, k) {
+    if(k === 0 || n === k) {
+        return 1;
+    }
+
+    if(k > n) {
+        return 0;
+    }
+
+    return this.factorial(n) / (this.factorial(k) * this.factorial(n - k));
+};
+
+//Hypergeometric, equal to target successes
+this.hyp = function(pop, pSuc, sample, tSuc) {
+    return ( this.binomialCoeff(pSuc, tSuc) * this.binomialCoeff(pop - pSuc, sample - tSuc) ) / this.binomialCoeff(pop, sample);
+};
+
+//Hypergeometric, less than target successes
+this.hyplt = function(pop, pSuc, sample, tSuc) {
+    return _.reduce(_.range(tSuc), function(sum, i) {
+        return sum + this.hyp(pop, pSuc, sample, i);
+    }, 0);
+};
+
+//Hypergeometric, less than or equal to target successes
+this.hyplte = function(pop, pSuc, sample, tSuc) {
+
+};
+
+//Hypergeometric, greater than target successes
+this.hypgt = function(pop, pSuc, sample, tSuc) {
+
+};
+
+//Hypergeometric, greater than or equal to target successes
+this.hypgte = function(pop, pSuc, sample, tSuc) {
+
+};
+
+//Exports
+module.exports = this;
